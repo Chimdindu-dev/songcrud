8951864b843cffdd55b94b4ef5cd9c7aec702e98
Chimdindu-dev <cchimdindu@gmail.com>
2022-11-04 03:08:23 +0200

    update

---
+++ songcrud/musicapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.song_list,name='song-list'),
+    path('create/',views.song_create,name='create- song'),
+    path('edit/',views.song_edit,name='edit-song'),
+    path('delete/',views.song_delete,name='delete-song'),
+]
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Song
 
 # Create your views here.
+def song_list(request):
+    song = Song.objects.all()
+    context = {
+        'song' : song,
+    }
+    return render(request,'musicapp/list.html',context)
+
+def song_create(request):
+    return render(request,'musicapp/create.html')
+
+def song_edit(request,pk):
+    return render(request,'musicapp/edit.html')
+
+def song_delete(request,pk):
+    return render(request,'musicapp/delete.html')
+
